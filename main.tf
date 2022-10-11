da7cb1c1a5f267cc13a22a27bbdd1befa1f13842
Mustafa Ali <mustafaali1128@gmail.com>
2022-10-11 19:25:26 +0000

    update

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@ resource "aws_s3_bucket_acl" "example_acl" {
   bucket = aws_s3_bucket.bucket2.id
   acl    = "public-read"
 }
+
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.example.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
